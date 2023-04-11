82cf317b28e3b86c55fadc8b65d2b5d054e4252e
xiayuanhao <2335961613@qq.com>
2023-04-12 00:07:34 +0800

    路由页面整理

--- src/icons/index.js
+++ src/icons/index.js
@@ -7,3 +7,4 @@ Vue.component('svg-icon', SvgIcon)
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
+// 以上两行代码的含义是，将所有的svg目录下的.svg后缀的文件全部引入到项目中
